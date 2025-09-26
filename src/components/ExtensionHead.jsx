1a63a750706dc3fc37a5c6f3a46f9469132f5168
Kingsley6624 <chineduezenwafor2@gmail.com>
2025-09-26 22:52:10 +0100

    edit the extensionhead fr responsiveness

--- src/components/ExtensionHead.jsx
+++ src/components/ExtensionHead.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const ExtensionHead = ({setFilter}) => {
 
   return (
-    <div className="flex justify-between">
+    <div className="flex flex-col md:flex-row items-center justify-between gap-3">
       <div>
         <h1 className="text-[var(--text-color)] font-bold text-3xl">Extension List</h1>
       </div>
       <div className="flex gap-4">
-        <button onClick={()=>setFilter('all')} className="bg-[var(--btn-bg)] text-[var(--text-color)] px-4 py-1 rounded-full border border-[var(--border)] shadow">All</button>
-        <button  onClick={()=>setFilter('active')} className="bg-[var(--btn-bg)] text-[var(--text-color)] px-4 py-1 rounded-full border border-[var(--border)] shadow">Active</button>
-        <button  onClick={()=>setFilter('inActive')} className="bg-[var(--btn-bg)] text-[var(--text-color)] px-4 py-1 rounded-full border border-[var(--border)] shadow">Inactive</button>
+        <button onClick={()=>setFilter('all')} className="bg-[var(--btn-bg)] text-[var(--text-color)] px-3 md:px-4 py-1 rounded-full border border-[var(--border)] shadow">All</button>
+        <button  onClick={()=>setFilter('active')} className="bg-[var(--btn-bg)] text-[var(--text-color)] px-3 md:px-4 py-1 rounded-full border border-[var(--border)] shadow">Active</button>
+        <button  onClick={()=>setFilter('inActive')} className="bg-[var(--btn-bg)] text-[var(--text-color)] px-3 md:px-4 py-1 rounded-full border border-[var(--border)] shadow">Inactive</button>
       </div>
     </div>
   );
